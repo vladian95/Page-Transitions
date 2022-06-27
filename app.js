41faf11502043d05aa667ac0e8f0945542d0fd09
vladian95 <kvankvan1995@gmail.com>
2022-06-27 13:32:07 +0300

    added transition effect

---
+++ app.js
@@ -0,0 +1,20 @@
+// Run animations
+barba.init({
+    preventRunning: true,
+    transitions: [
+        //showcase transitions
+        {
+            name: "default",
+            leave(data){
+                const done = this.async();
+                let current = data.current.container;
+                gsap.fromTo(current, {opacity: 1}, {opacity: 0, duration: 1, onComplete: done });
+            },
+            enter(data){
+                const done = this.async();
+                let next = data.next.container;
+                gsap.fromTo(next, {opacity: 0}, {opacity: 1, duration: 1, onComplete: done });
+            },
+        },
+    ],
+})
